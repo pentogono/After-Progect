3ec402fbd67db1ddefd96877bd698bd100324be1
pentogono <ilg3109@gmail.com>
2019-02-11 23:32:25 +0300

    привязка iframe

--- src/main/resources/sql/data-h2.sql
+++ src/main/resources/sql/data-h2.sql
@@ -1,9 +1,9 @@
 INSERT INTO brevets (id, name, link_map) VALUES
-(1, 'Первый', 'www.gpsies.com/mapOnly.do?fileId=txcfvdiojcukbuum'),
-(2, 'Майский', 'www.gpsies.com/mapOnly.do?fileId=sxawajdfjpqahyzf'),
-(3, 'Июньский', 'www.gpsies.com/mapOnly.do?fileId=ddegqfsoxuuxwmgq'),
-(4, 'Июльский', 'www.gpsies.com/mapOnly.do?fileId=ddegqfsoxuuxwmgq'),
-(5, 'Августовский 600', 'www.gpsies.com/mapOnly.do?fileId=kmendsvjjkzqbmzh'),
-(6, 'Августовский 200', 'www.gpsies.com/mapOnly.do?fileId=soddtrebeqisvkhl'),
-(7, 'Марийский', 'www.gpsies.com/mapOnly.do?fileId=yhcnunezuurwxchc');
+(1, 'Первый', '//www.gpsies.com/mapOnly.do?fileId=txcfvdiojcukbuum'),
+(2, 'Майский', '//www.gpsies.com/mapOnly.do?fileId=sxawajdfjpqahyzf'),
+(3, 'Июньский', '//www.gpsies.com/mapOnly.do?fileId=ddegqfsoxuuxwmgq'),
+(4, 'Июльский', '//www.gpsies.com/mapOnly.do?fileId=ddegqfsoxuuxwmgq'),
+(5, 'Августовский 600', '//www.gpsies.com/mapOnly.do?fileId=kmendsvjjkzqbmzh'),
+(6, 'Августовский 200', '//www.gpsies.com/mapOnly.do?fileId=soddtrebeqisvkhl'),
+(7, 'Марийский', '//www.gpsies.com/mapOnly.do?fileId=yhcnunezuurwxchc');
 
